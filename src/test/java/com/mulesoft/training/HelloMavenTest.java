7548c2c30e6c4538e4ca34be17129a00f662e43b
Natalya Ilyashenko <natalyailyashenko@Natalyas-MacBook-Air.local>
2017-07-25 11:39:35 -0500

    Added Content-Type header to response

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,17 +8,13 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven");
-    }
-    @Test
-    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-      MuleEvent event = runFlow("retrieveFlights");
-      String contentType = event.getMessage().getOutboundProperty("Content-Type");
-      assertEquals("application/json", contentType);
-    }
-    
+	@Test
+	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+	   MuleEvent event = runFlow("retrieveFlights");
+	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+	   assertEquals("application/json", contentType);
+	 }
+	
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
